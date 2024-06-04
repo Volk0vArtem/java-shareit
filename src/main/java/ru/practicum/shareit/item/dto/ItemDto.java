8ee511737a9433facb918e868e114ded759a2b12
Artem <artem33volkov@gmail.com>
2024-06-04 11:46:29 +0300

    feat: add ItemDto and UserDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.request.ItemRequest;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDto {
+    private String name;
+    private String description;
+    private Boolean available;
+    private Long owner;
+    private ItemRequest itemRequest;
 }
