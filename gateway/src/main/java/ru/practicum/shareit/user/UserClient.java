f225cb90a85ecb88ad0b6fb6e33e260617cc8157
Artem <artem33volkov@gmail.com>
2024-07-16 11:23:30 +0300

    feat: add ItemRequest gateway

--- gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import ru.practicum.shareit.user.dto.UserRequestDto;
 public class UserClient extends BaseClient {
     private static final String API_PREFIX = "/users";
 
+    @Autowired
     public UserClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
         super(
                 builder
